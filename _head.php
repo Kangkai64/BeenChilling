b550768224d14f0a2e4b61d197f6e465be14fc0e
KangKai <hokk-wp23@student.tarc.edu.my>
2025-02-25 12:17:09 +0800

    Edited the beenchilling.js file path

--- _head.php
+++ _head.php
@@ -12,7 +12,7 @@
     <title><?= $_title ?? "Untitled" ?></title>
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" type="text/css" href="../css/main.css">
-    <script src="js/beenchilling.js"></script>
+    <script src="../js/beenchilling.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/d743fd0ad4.js" crossorigin="anonymous"></script>
     <!-- Google tag (gtag.js) -->
